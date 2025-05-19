6d916e3c59e2a37d73d0cc74bdb21914ee393366
sbolano <sbolano@estudiante.uniajc.edu.co>
2025-05-19 16:55:39 +0000

    Verificador

---
+++ Verificadordenumerosprimos_POO/Verificadordenumerosprimos_POO.java
@@ -0,0 +1,12 @@
+package Verificadordenumerosprimos_POO;
+
+
+public class Verificadordenumerosprimos_POO {
+
+    public String Verificadordenuemrosprimos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Verificadordenuemrosprimos'");
+    }
+
+}
+
